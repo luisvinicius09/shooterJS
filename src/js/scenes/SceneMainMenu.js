8f913ff9a683be35b7b20c5e40391fdb6bc4666e
Luis Vinicius <luisvinicius0906@gmail.com>
2021-05-06 19:22:28 -0300

    Fix logic to start the game and add background

--- src/js/scenes/SceneMainMenu.js
+++ src/js/scenes/SceneMainMenu.js
@@ -1,3 +1,5 @@
+import { ScrollingBackground } from "./utils/ScrollingBackground";
+
 export default class SceneMainMenu extends Phaser.Scene {
   constructor() {
     super({
@@ -6,14 +8,76 @@ export default class SceneMainMenu extends Phaser.Scene {
   };
 
   preload() {
-    
+    this.load.image('sprBtnPlay', './img/buttons/sprBtnPlay.png');
+    this.load.image('sprBtnPlayHover', './img/buttons/sprBtnPlayHover.png');
+    this.load.image('sprBtnPlayDown', './img/buttons/sprBtnPlayDown.png');
+    this.load.image('sprBtnRestart', './img/buttons/sprBtnRestart.png');
+    this.load.image('sprBtnRestartHover', './img/buttons/sprBtnRestartHover.png');
+    this.load.image('sprBtnRestartDown', './img/buttons/sprBtnRestartDown.png');
+
+    this.load.audio('sndBtnOver', './audio/buttons/sndBtnOver.wav');
+    this.load.audio('sndBtnDown', './audio/buttons/sndBtnDown.wav');
+
+    this.load.image('sprBg0', './img/background.png');
+    this.load.image('sprBg1', './img/sprBg1.png');
+    this.load.image('sprBg2', './img/sprBg2.png');
   };
 
   create() {
-    this.scene.start('SceneMain');
+    this.sfx = {
+      btnOver: this.sound.add('sndBtnOver'),
+      btnDown: this.sound.add('sndBtnDown'),
+    };
+
+    this.btnPlay = this.add.sprite(
+      this.game.config.width * 0.5,
+      this.game.config.height * 0.5,
+      'sprBtnPlay',
+    );
+    this.btnPlay.setInteractive();
+
+    this.btnPlay.on('pointerover', () => {
+      this.btnPlay.setTexture('sprBtnPlayHover');
+      this.sfx.btnOver.play();
+    }, this);
+
+    this.btnPlay.on('pointerout', () => {
+      this.btnPlay.setTexture('sprBtnPlay');
+    });
+
+    this.btnPlay.on('pointerdown', () => {
+      this.btnPlay.setTexture('sprBtnPlayDown');
+      this.sfx.btnDown.play();      
+    }, this);
+
+    this.btnPlay.on('pointerup', () => {
+      this.btnPlay.setTexture('sprBtnPlay');
+      this.scene.start('SceneMain');
+    }, this);
+
+    this.title = this.add.text(this.game.config.width * .5, 128, 'SHOOTERJS', {
+      fontFamily: 'monospace',
+      fontSize: 60,
+      fontStyle: 'bold',
+      color: '#fff',
+      align: 'center',
+    });
+    this.title.setOrigin(.5);
+
+    this.backgrounds = [];
+    for (let i = 0; i < 5; i += 1) {
+      let bg = new ScrollingBackground(this, 'sprBg1', -(i * 10));
+      this.backgrounds.push(bg);
+    }
+
+    let bg = this.add.sprite(0, 0, 'sprBg0');
+    bg.setDepth(-10);
+
   };
 
   update() {
-
+    for (let i = 0; i < this.backgrounds.length; i += 1) {
+      this.backgrounds[i].update();
+    }
   };
 }
