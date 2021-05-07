4faad79330cea35f717efafe03ad31a5398b393d
Luis Vinicius <luisvinicius0906@gmail.com>
2021-05-07 18:06:37 -0300

    Add leaderboard title

--- src/js/scenes/SceneLeaderboard.js
+++ src/js/scenes/SceneLeaderboard.js
@@ -10,7 +10,22 @@ export default class SceneLeaderboard extends Phaser.Scene {
   };
 
   create() {
+    this.title = this.add.text(this.game.config.width * .5, 128, 'LEADERBOARD', {
+      fontFamily: 'monospace',
+      fontSize: 60,
+      fontStyle: 'bold',
+      color: '#fff',
+      align: 'center',
+    });
+    this.title.setOrigin(.5);
 
+
+    this.btnMenu = this.add.sprite({
+
+    })
+    this.btnMenu.setInteractive();
+
+    
   };
 
   update() {
