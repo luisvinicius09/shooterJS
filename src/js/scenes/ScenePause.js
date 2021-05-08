512d907d81887d8722695fc015848bc882157290
Luis Vinicius <luisvinicius0906@gmail.com>
2021-05-08 09:04:19 -0300

    Update images and paths of it

--- src/js/scenes/ScenePause.js
+++ src/js/scenes/ScenePause.js
@@ -6,15 +6,22 @@ export default class ScenePause extends Phaser.Scene {
   };
 
   preload() {
-    this.load.image('sprBtnResume', './img/buttons/sprBtnRestart.png');
+    this.load.image('sprBtnResume', './img/buttons/sprBtnResume.png');
     this.load.image('sprBtnResumeHover', './img/buttons/sprBtnResumeHover.png');
-    this.load.image('sprBtnResumeDown', './img/buttons/sprBtnResumeDown,png');
+    this.load.image('sprBtnResumeDown', './img/buttons/sprBtnResumeDown.png');
 
     this.load.audio('sndBtnOver', './audio/buttons/sndBtnOver.wav');
     this.load.audio('sndBtnDown', './audio/buttons/sndBtnDown.wav');
   };
 
   create() {
+    this.keyEsc = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
+
+    this.sfx = {
+      btnOver: this.sound.add('sndBtnOver'),
+      btnDown: this.sound.add('sndBtnDown'),
+    };
+
     this.title = this.add.text(this.game.config.width * .5, 128, 'PAUSED', {
       fontFamily: 'monospace',
       fontSize: 60,
@@ -31,15 +38,17 @@ export default class ScenePause extends Phaser.Scene {
     this.btnResume.setInteractive();
 
     this.btnResume.on('pointerover', () => {
-
+      this.btnResume.setTexture('sprBtnResumeHover');
+      this.sfx.btnOver.play();
     }, this);
 
     this.btnResume.on('pointerout', () => {
-
+      this.btnResume.setTexture('sprBtnResume');
     }, this);
 
     this.btnResume.on('pointerdown', () => {
-
+      this.btnResume.setTexture('sprBtnResumeDown');
+      this.sfx.btnDown.play();
     }, this);
 
     this.btnResume.on('pointerup', () => {
@@ -48,4 +57,11 @@ export default class ScenePause extends Phaser.Scene {
     }, this);
 
   };
+
+  update() {
+    if (this.keyEsc.isDown) {
+      this.scene.resume('SceneMain');
+      this.scene.stop();
+    }
+  }
 }
